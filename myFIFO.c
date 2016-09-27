d43cf04c8527d5afe57da5cf9cd129741434ea7a
PhamNhatTan <baoden93@gmail.com>
2016-09-27 11:26:37 +0700

    add FiFo to uart communication, decrease MCU process

---
+++ myFIFO.c
@@ -0,0 +1,154 @@
+/*
+ * myFIFO.c
+ *
+ *  Created on: Oct 21, 2015
+ *      Author: NhatTan
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/uart.h"
+#include "inc/hw_memmap.h"
+#include "utils/ringbuf.h"
+#include "myFIFO.h"
+#include "serial.h"
+
+//uint8_t TxPcBuffer[128],RxPcBuffer[128];
+//tRingBufObject TxRingBuf;
+//tRingBufObject RxRingBuf;
+
+FIFO communicationFIFO;
+FIFO kinectFIFO;
+
+void myFIFO_init(FIFO *fifo)
+{
+	//
+	// Initialize the ring buffer.
+	//
+	RingBufInit(&(fifo->Tx), fifo->TxBuffer, sizeof(fifo->TxBuffer));
+	RingBufInit(&(fifo->Rx), fifo->RxBuffer, sizeof(fifo->RxBuffer));
+}
+
+void FIFO_Rx_CharPut(FIFO *fifo,char ucData)
+{
+	//	if(UARTCharsAvail(UART_BASE))
+	RingBufWriteOne(&(fifo->Rx),ucData);
+}
+
+void FIFO_Rx_StrGet(FIFO *fifo,char *fifo_str)
+{
+	//	if(UARTCharsAvail(UART_BASE))
+	*fifo_str=(char)RingBufReadOne(&(fifo->Rx));
+	while (*fifo_str != TERMINATOR_)
+	{
+		fifo_str++;
+		*fifo_str= (char)RingBufReadOne(&(fifo->Rx));
+	}
+	*(fifo_str+1)='\0';
+}
+
+void update_hardwareFIFO(FIFO *fifo,uint32_t ui32Base)
+{
+	if(!UARTBusy(ui32Base))//hardwareFIFO full 16 bytes
+	{
+		uint8_t SoftFIFO_availData;
+		uint8_t buffer[20];
+		uint8_t i;
+
+		SoftFIFO_availData=RingBufUsed(&(fifo->Tx));//check how many data available in FIFO
+		if(SoftFIFO_availData>16)
+		{
+			SoftFIFO_availData=16;
+		}
+
+		RingBufRead(&(fifo->Tx), buffer, SoftFIFO_availData);
+		for( i=0;i<SoftFIFO_availData;i++)
+		{
+			UARTCharPut(ui32Base,*(buffer+i) );
+		}
+	}
+}
+
+
+
+void FIFOCharPut(FIFO *fifo,uint32_t ui32Base,char ucData,bool update)
+{
+	RingBufWriteOne(&(fifo->Tx),ucData);
+	if(update)
+		update_hardwareFIFO(fifo,ui32Base);
+}
+
+void FIFO_PutStr(FIFO *fifo,uint32_t ui32Base,char *uart_str,bool update,bool terminator)
+{
+	while(*uart_str != '\0') {RingBufWriteOne(&(fifo->Tx),*uart_str++);}
+	if(terminator)
+		RingBufWriteOne(&(fifo->Tx),TERMINATOR_);
+	if(update)
+		update_hardwareFIFO(fifo,ui32Base);
+}
+
+
+
+void FIFO_3_float_display(float num1, float num2, float num3 )
+{
+	char strNum[10];
+	float2str(num1,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,false,false);
+	FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	float2str(num2,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,false,false);
+	FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	float2str(num3,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,true,true);
+}
+
+void FIFO_float_display( float num)
+{
+	char strNum[10];
+	float2str(num,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,true,true);
+}
+
+void FIFO_int_display( float num)
+{
+	char strNum[10];
+	int2str(num,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,true,true);
+}
+
+void FIFO_3_int_display(int16_t num1, int16_t num2, int16_t num3 )
+{
+	char strNum[10];
+	int2str(num1,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,false,false);
+	FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	int2str(num2,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,false,false);
+	FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	int2str(num3,strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,true,true);
+}
+
+void FIFO_n_int_display(uint32_t *num,uint8_t size)
+{
+	char strNum[10];
+	uint8_t i;
+	FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	for (i=1;i<size;i++)
+	{
+		int2str(*(num++),strNum);
+		FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,false,false);
+		FIFOCharPut(&communicationFIFO,UART_BASE,'a',false);
+	}
+	int2str(*(num),strNum);
+	FIFO_PutStr(&communicationFIFO,UART_BASE,strNum,true,true);
+}
+
+
+void printf(char *uart_str)
+{
+	FIFO_PutStr(&communicationFIFO,UART_BASE,uart_str,true,true);
+}
+void writeByte(char ucData)
+{
+	FIFOCharPut(&communicationFIFO,UART_BASE,ucData,false);
+}
---
+++ myFIFO.h
@@ -0,0 +1,45 @@
+/*
+ * myFIFO.h
+ *
+ *  Created on: Oct 21, 2015
+ *      Author: NhatTan
+ */
+
+#ifndef MYFIFO_H_
+#define MYFIFO_H_
+
+#define FIFO_BASE
+
+//#define USE_TXFIFO_
+//extern tRingBufObject sRingBuf;
+typedef struct{
+	tRingBufObject Tx;
+	tRingBufObject Rx;
+	uint8_t TxBuffer[128];
+	uint8_t RxBuffer[128];
+} FIFO;
+
+extern FIFO communicationFIFO;
+extern FIFO kinectFIFO;
+
+extern char pcBuffer[128],pcData[20];
+
+
+extern void myFIFO_init(FIFO *fifo);
+
+extern void FIFO_Rx_CharPut(FIFO *fifo,char ucData);
+extern void FIFO_Rx_StrGet(FIFO *fifo,char *fifo_str);
+
+extern void update_hardwareFIFO(FIFO *fifo,uint32_t ui32Base);
+extern void FIFOCharPut(FIFO *fifo,uint32_t ui32Base,char ucData,bool update);
+extern void FIFO_PutStr(FIFO *fifo,uint32_t ui32Base,char *uart_str,bool update,bool terminator);
+
+extern void FIFO_3_float_display(float num1, float num2, float num3 );
+extern void FIFO_float_display( float num);
+extern void FIFO_3_int_display(int16_t num1, int16_t num2, int16_t num3 );
+extern void FIFO_n_int_display(uint32_t *num,uint8_t size);
+
+extern void printf(char *uart_str);
+extern void writeByte(char ucData);
+
+#endif /* MYFIFO_H_ */
